7777e6fea180aeafa376549e5ce51e95cfb996fb
dilarakiliccodes <dilarakilic610@gmail.com>
2026-07-01 16:19:33 +0300

    Add user model

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from backend.core.config import settings
+from backend.database.connection import Base, engine
+from backend.models.user import User
+
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
 )
 
+Base.metadata.create_all(bind=engine)
 @app.get("/")
 def home():
     return {
